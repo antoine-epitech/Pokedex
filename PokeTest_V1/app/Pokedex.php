7e52eae705599e3a5e292c6b003e4d390cc6ad09
antoine-epitech <antoine.meyniel@epitech.eu>
2020-07-29 16:16:26 +0200

    routes ok sauf /pokemons -> JSON format

---
+++ PokeTest_V1/app/Pokedex.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pokedex extends Model
+{
+    protected $fillable = [
+        'id_pok', 'nom_pok',
+    ];
+
+    public function types(){
+        return $this->hasMany('App\Types');     
+    }
+}
---
+++ PokeTest_V1/app/Types.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Types extends Model
+{
+    protected $fillable = ['id_pok','type1', 'type2'];
+
+    public function pokedex() 
+	{
+		return $this->belongsTo('App\Pokedex','id_pok','id_pok');
+	}
+}
